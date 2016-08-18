70d5f9a439b23f23d3d390fed59049725b5a44d0
ReeceStevens <reecestevens24@gmail.com>
2016-08-18 00:11:53 -0500

    Restore interface changes from previous work

--- inc/interface.h
+++ inc/interface.h
@@ -8,8 +8,8 @@
 #define DISPLAY_RESET PC5
 #define DISPLAY_SPI spi_c1
 
-#define SCREEN_ROWS 10
-#define SCREEN_COLUMNS 10
+#define SCREEN_ROWS 12
+#define SCREEN_COLUMNS 12
 
 enum layout{
 	home, settings	
@@ -34,19 +34,19 @@ void cancelAlarmChanges(void) {
 
 void restoreDefaultAlarms(void) {};
 
-Button settings_button = Button(9,9,2,2,RA8875_RED,"Alarm Settings",
+Button settings_button = Button(11,11,2,2,RA8875_RED,"Alarm Settings",
                          true,&tft,&openSettings);
-Button confirm_button = Button(9,1,2,2,RA8875_GREEN,"Confirm",
+Button confirm_button = Button(11,9,2,2,RA8875_GREEN,"Confirm",
                         true,&tft,&saveAlarmChanges);
-Button cancel_button = Button(9,9,2,2,RA8875_RED,"Cancel",
+Button cancel_button = Button(5,9,2,2,RA8875_RED,"Cancel",
                         true,&tft,&cancelAlarmChanges);
 Button default_button = Button(6,7,2,2,RA8875_LIGHTGREY,"Default Settings",
                         true,&tft,&restoreDefaultAlarms);
-TextBox title = TextBox(1,3,1,3,RA8875_BLACK,RA8875_WHITE,3,true,false,
+TextBox title = TextBox(12,1,1,3,RA8875_BLACK,RA8875_WHITE,3,true,false,
                         "FreePulse Patient Monitor v0.9", &tft);
-TextBox hr_label = TextBox(2,8,3,3,RA8875_BLACK,RA8875_BLUE,2,true,
+TextBox hr_label = TextBox(1,10,4,3,RA8875_BLACK,RA8875_BLUE,2,true,
                             true,"BPM",&tft);
-LargeNumberView heartrate = LargeNumberView(3,8,2,3,RA8875_BLACK,
+LargeNumberView heartrate = LargeNumberView(3,10,2,3,RA8875_BLACK,
                             RA8875_BLUE,true,60,&tft);
 
 void composeMainScreen(Screen& s) {
