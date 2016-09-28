0c2596f9c2142d8ba52065132a2b59745cf41fb2
ReeceStevens <reecestevens24@gmail.com>
2016-09-28 10:09:36 -0500

    Adjust display position for pulse ox

--- inc/Signals.h
+++ inc/Signals.h
@@ -5,7 +5,7 @@
 SPI_Interface afe4400_spi(spi_c2);
 
 ECG ecg = ECG(1,1,4,9,PB0,4096,RA8875_BLUE,RA8875_BLACK,1000,tim3,&tft);
-PulseOx spo2 = PulseOx(6,1,3,3,&afe4400_spi,PC8,PA9,PA8,PB7,&tft);
+PulseOx spo2 = PulseOx(5,1,4,9,&afe4400_spi,PC8,PA9,PA8,PB7,&tft);
 
 void enableSignalAcquisition(void) {
     spo2.enable();
